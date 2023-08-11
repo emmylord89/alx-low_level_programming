34bfcc2ac14261fdd85a4f28a15f3f00495b6ccf
emmylord89 <pemmy80s@gmail.com>
2023-08-10 18:53:51 -0700

    x0C-more_malloc_free

--- x0C-more_malloc_free/main.h
+++ x0C-more_malloc_free/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -14,5 +12,6 @@ int _strlen(char *s);
 int is_digit(char *s);
 int _isdigit(int c);
 char *big_multiply(char *s1, char *s2);
+
 #endif
 
