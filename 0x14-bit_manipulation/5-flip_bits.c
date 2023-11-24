348d038f68a68c47b9413669a543e6753a87619d
emmylord89 <pemmy80s@gmail.com>
2023-11-24 10:33:57 +0000

    New Commit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,9 +16,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 		for (i = 63; i >= 0; i--)
 		{
-				current = exclusive >> i;
-				if (current & 1)
-						count++;
+			current = exclusive >> i;
+			if (current & 1)
+			count++;
 		}
 
 		return (count);
